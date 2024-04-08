3a5281fcee8731a6b4eb8fc0ed1562fd722ada4f
Coyote <Coyote@gmail.com>
2024-04-08 23:24:41 +0300

    Initial Commit

---
+++ New_App_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  New_App_2
+//
+//  Created by SERGEI OSKIN on 08.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
